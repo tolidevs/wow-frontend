695a423f17d641de3c107105182be9d0913b8473
flatironschool <t.benson@live.co.uk>
2020-03-18 14:40:41 +0000

    rendering services on results and show pages

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -2,72 +2,118 @@ import React, { Component } from "react";
 import {
     Item,
     Grid,
-    Icon
+    Icon,
+    Image
 } from "semantic-ui-react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
+import netflix from "../images/netflix-logo.png";
+import amazon from "../images/amazon-icon.png";
+import disney from "../images/disneyplus-logo.jpg";
+import itunes from "../images/itunes-logo.jpg";
+import google from "../images/google-play-logo.png";
+import stream from "../images/stream.png";
 
 
 class ResultCard extends Component {
-  state = {
-    selected: false
-  };
+    state = {
+        selected: false
+    };
 
-  handleClick = imdbID => {
-    this.props.setSelectedShow(imdbID);
-    this.setState({
-      selected: true
-    });
-    console.log(imdbID);
-  };
+    handleClick = imdbID => {
+        this.props.setSelectedShow(imdbID);
+        this.setState({
+        selected: true
+        });
+        console.log(imdbID);
+    };
 
-  redirectToShow = () => {
-    return this.state.selected && <Redirect to="/results/show" />;
-  };
+    redirectToShow = () => {
+        return this.state.selected && <Redirect to="/results/show" />;
+    };
 
-  renderIcon = type => {
-    if (type === "movie") {
-      return <Icon name="film" size="big" floated="right" />;
-    } else if (type === "series") {
-      return <Icon name="tv" size="big" floated="right" />;
-    } else {
-      return <Icon name="question" size="big" floated="right" />;
-    }
-  };
+    renderIcon = type => {
+        if (type === "movie") {
+        return <Icon name="film" size="big" floated="right" />;
+        } else if (type === "series") {
+        return <Icon name="tv" size="big" floated="right" />;
+        } else {
+        return <Icon name="question" size="big" floated="right" />;
+        }
+    };
 
     save = (imdbID, title, type, year, poster) => {
-       return this.props.user && this.props.saveShow(imdbID, title, type, year, poster);
-  };
+        return this.props.user && this.props.saveShow(imdbID, title, type, year, poster);
+    };
 
-  render() {
-    const { imdbID, title, type, year, poster, services } = this.props;
+    renderServices = (services) => {
+        return services.map(
+            service => {
+                switch (service.name) {
+                default:
+                    return null;
+                case "Netflix":
+                    return (
+                        <Image src={netflix} as='a' href={service.url}></Image>
+                    )
+                case "iTunes":
+                    return (
+                      <Image src={itunes} as="a" href={service.url}></Image>
+                    );
+                case "Amazon Instant Video":
+                    return (
+                      <Image src={amazon} as="a" href={service.url}></Image>
+                    );
+                case "Google Play":
+                    return (
+                      <Image src={google} as="a" href={service.url}></Image>
+                    );
+                case "DisneyPlus":
+                    return (
+                      <Image src={disney} as="a" href={service.url}></Image>
+                    );
+                case "other":
+                    return (
+                      <Image src={stream} as="a" href={service.url}></Image>
+                    );
+                }
+            }
+        )
+    }
 
-    return (
-      <Grid.Row key={imdbID}>
-        <Item.Group style={{ width: "90vw" }}>
-          <Item.Header onClick={() => this.handleClick(imdbID)}>
-            {title}
-          </Item.Header>
-          <Item.Image
-            src={poster}
-            bordered
-            centered
-            onClick={() => this.handleClick(imdbID)}
-          />
-          <Item.Meta>{year}</Item.Meta>
-          <Item.Extra>
-            <Icon
-              name="heart outline"
-              size="big"
-              onClick={() => this.save(imdbID, title, type, year, poster)}
-            />
-            {this.renderIcon(type)}
-          </Item.Extra>
-        </Item.Group>
-        {this.redirectToShow()}
-      </Grid.Row>
-    );
-  }
+    render() {
+        const { imdbID, title, type, year, poster, services } = this.props;
+
+        return (
+          <Grid.Row key={imdbID}>
+            <Item.Group style={{ width: "90vw" }}>
+              <Item.Header onClick={() => this.handleClick(imdbID)}>
+                {title}
+              </Item.Header>
+              <Item.Image
+                src={poster}
+                bordered
+                centered
+                onClick={() => this.handleClick(imdbID)}
+              />
+              <Item.Meta>{year}</Item.Meta>
+              <Item.Extra>
+                <Icon
+                  name="heart outline"
+                  size="big"
+                  onClick={() => this.save(imdbID, title, type, year, poster)}
+                />
+                {this.renderIcon(type)}
+              </Item.Extra>
+              <Item.Extra>
+                Watch on: <br></br>
+                <Image.Group size='tiny'>{this.renderServices(services)}</Image.Group>
+              </Item.Extra>
+            </Item.Group>
+            {this.redirectToShow()}
+          </Grid.Row>
+        );
+    }
 }
 
 const mapStateToProps = ({ user }) => {
--- src/containers/ShowPage.js
+++ src/containers/ShowPage.js
@@ -7,76 +7,104 @@ import {
 } from "semantic-ui-react";
 import { connect } from "react-redux";
 import API from "../API";
+import netflix from "../images/netflix-logo.png";
+import amazon from "../images/amazon-icon.png";
+import disney from "../images/disneyplus-logo.jpg";
+import itunes from "../images/itunes-logo.jpg";
+import google from "../images/google-play-logo.png";
+import stream from "../images/stream.png";
 
 
 class ShowPage extends Component {
-    state = {
-        show: null
-    }
-    
-    getShowShortDetails = () => {
-        const { selected_show, search_results } = this.props;
-        return [...search_results].filter(show => show.imdbID === selected_show)
-    };
-        
-    componentDidMount() {
-        this.setState({
-          show: this.getShowShortDetails()[0]
-        });
-        API.getShowDetails(this.props.selected_show)
-            .then( showObj => this.props.setShowDetails({...this.state.show, ...showObj}))
-    }
-        
-    renderType = type => {
-        if (type === "movie") {
-            return (
-            <Fragment>
-                <Icon name="film" size="big" floated="right" />
-                Film
-            </Fragment>
-            );
-                    
-        } else if (type === "series") {
-            return (
-                <Fragment>
-                    <Icon name="tv" size="big" floated="right" />
-                    Series
-                </Fragment> 
-            )
-        } else {
-        return <Icon name="question" size="big" floated="right" />
-        }
-    };
+  state = {
+    show: null
+  };
 
-    render() {
-        const { show_details } = this.props;
-        const {
-        title,
-        type,
-        year,
-        poster,
-        services
-        } = this.getShowShortDetails()[0]
-        return (
-          <Segment style={{ width: "90vw" }} verticalAlign="middle">
-            <Header>{title}</Header>
-            <Image src={poster} bordered centered />
-            <Icon name="heart outline" size="big" floated="right" />
-            <br></br>Watch on:
-            <br></br>
-            {this.renderType(type)} {year}
-            {show_details && (
-              <div>
-                <p>
-                  <Icon type="question" size="big"></Icon> {show_details.genre}
-                </p>
-                <p>IMDB Rating: {show_details.imdbRating} / 10</p>
-                <p>{show_details.plot}</p>
-              </div>
-            )}
-          </Segment>
-        );
+  getShowShortDetails = () => {
+    const { selected_show, search_results } = this.props;
+    return [...search_results].filter(show => show.imdbID === selected_show);
+  };
+
+  componentDidMount() {
+    this.setState({
+      show: this.getShowShortDetails()[0]
+    });
+    API.getShowDetails(this.props.selected_show).then(showObj =>
+      this.props.setShowDetails({ ...this.state.show, ...showObj })
+    );
+  }
+
+  renderType = type => {
+    if (type === "movie") {
+      return (
+        <Fragment>
+          <Icon name="film" size="big" floated="right" />
+          Film
+        </Fragment>
+      );
+    } else if (type === "series") {
+      return (
+        <Fragment>
+          <Icon name="tv" size="big" floated="right" />
+          Series
+        </Fragment>
+      );
+    } else {
+      return <Icon name="question" size="big" floated="right" />;
     }
+  };
+
+  renderServices = services => {
+    return services.map(service => {
+      switch (service.name) {
+        default:
+          return null;
+        case "Netflix":
+          return <Image src={netflix} as="a" href={service.url}></Image>;
+        case "iTunes":
+          return <Image src={itunes} as="a" href={service.url}></Image>;
+        case "Amazon Instant Video":
+          return <Image src={amazon} as="a" href={service.url}></Image>;
+        case "Google Play":
+          return <Image src={google} as="a" href={service.url}></Image>;
+        case "DisneyPlus":
+          return <Image src={disney} as="a" href={service.url}></Image>;
+        case "other":
+          return <Image src={stream} as="a" href={service.url}></Image>;
+      }
+    });
+  };
+
+  render() {
+    const { show_details } = this.props;
+    const {
+      title,
+      type,
+      year,
+      poster,
+      services
+    } = this.getShowShortDetails()[0];
+    return (
+      <Segment style={{ width: "90vw" }} verticalAlign="middle">
+        <Header>{title}</Header>
+        <Image src={poster} bordered centered />
+        <Icon name="heart outline" size="big" floated="right" />
+        <br></br>Watch on:
+        <Image.Group size="tiny">{this.renderServices(services)}</Image.Group>
+        <br></br>
+        {this.renderType(type)} {year}
+        {show_details && (
+          <div>
+            <p>
+              <Icon type="question" size="big"></Icon> {show_details.genre}
+            </p>
+            <p>IMDB Rating: {show_details.imdbRating} / 10</p>
+            <p>{show_details.plot}</p>
+          </div>
+        )}
+      </Segment>
+    );
+  }
 }
 
     const mapStateToProps = ({ selected_show, search_results, show_details }) => {
